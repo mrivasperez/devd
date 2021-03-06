62dd2b8023985a811c9907486d39284b1fa4471d
Miguel Rivas Perez <mrivasperez@icloud.com>
2021-03-05 21:35:40 -0800

    updated post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -148,4 +148,83 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
+// @route           PUT api/posts/like/:id
+// @description     Unike post by ID
+// @access          Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    //   find post to be liked by ID
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(400).json({ message: "Post not found." });
+
+    //   check if the post is liked by this user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Post has not been liked by this user." });
+    }
+
+    // remove user to like list
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    // save like
+    await post.save();
+
+    // return liked user list
+    return res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send("Server Error");
+  }
+});
+
+// @route           POST api/posts/comment/:id
+// @description     Comment on a post
+// @access          Private
+router.post(
+  "/comment/:id",
+  [
+    auth,
+    // validate that text is not empty
+    [check("text", "Text is required.").not().isEmpty()],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const post = await Post.findById(req.params.id);
+
+      // create new post using Post model
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      // save new comment
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
